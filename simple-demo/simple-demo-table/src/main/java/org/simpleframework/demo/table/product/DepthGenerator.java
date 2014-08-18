1eb768f3c6a7f12ecab882a59458e755690f727e
Niall Gallagher <gallagher_niall@yahoo.com>
2014-08-18 22:20:43 +1000

    Ensure telemetry connection comes back online

--- simple-demo/simple-demo-table/src/main/java/org/simpleframework/demo/table/product/DepthGenerator.java
+++ simple-demo/simple-demo-table/src/main/java/org/simpleframework/demo/table/product/DepthGenerator.java
@@ -39,7 +39,7 @@ public class DepthGenerator extends Thread {
                
                createDepth(name, company, randomMid, randomSpread, randomVolume);               
             }            
-            Thread.sleep(1);
+            Thread.sleep(frequency);
          } catch(Exception e) {
             e.printStackTrace();          
          }
